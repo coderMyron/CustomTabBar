4c7a4684d2e7d928c95760b15cf3fd8743fc2239
coderMyron <244270327@qq.com>
2019-07-28 15:10:58 +0800

    Update MyTabBar.h

--- CustomTabBar/CustomTabBar/MyTabBar.h
+++ CustomTabBar/CustomTabBar/MyTabBar.h
@@ -3,7 +3,7 @@
 //  CustomTabBar
 //
 //  Created by Myron on 2019/7/27.
-//  Copyright © 2019 李木义. All rights reserved.
+//  Copyright © 2019 Myron. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
