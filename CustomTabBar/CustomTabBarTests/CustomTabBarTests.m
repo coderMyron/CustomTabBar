f4644c83f274d8d94d3fad3e88511489b92a6e28
coderMyron <244270327@qq.com>
2019-07-28 15:17:42 +0800

    Update CustomTabBarTests.m

--- CustomTabBar/CustomTabBarTests/CustomTabBarTests.m
+++ CustomTabBar/CustomTabBarTests/CustomTabBarTests.m
@@ -3,7 +3,7 @@
 //  CustomTabBarTests
 //
 //  Created by Myron on 2019/7/27.
-//  Copyright © 2019 李木义. All rights reserved.
+//  Copyright © 2019 Myron. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
